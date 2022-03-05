b53f6d8b2c116d3e9cc776b26bf2e454efc5cc6e
GitHub Action <action@github.com>
2022-03-05 16:00:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/takenbrandi/protanthemes/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/takenbrandi/protanthemes/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.takenbrandi.protanthemes.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.takenbrandi.protanthemes.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/takenbrandi/protanthemes/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/takenbrandi/protanthemes/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.takenbrandi.protanthemes.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.takenbrandi.protanthemes.MyBundle
 
 class MyApplicationService {
 
